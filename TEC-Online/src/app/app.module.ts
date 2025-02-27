a49251f2f4629c204239f14833fe25a727747fe7
claud2005 <162583107+claud2005@users.noreply.github.com>
2025-02-27 10:54:44 +0000

    .....

--- TEC-Online/src/app/app.module.ts
+++ TEC-Online/src/app/app.module.ts
@@ -5,17 +5,13 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule,
-    CommonModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
