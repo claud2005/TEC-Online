83b07fd21828b2cc332cbe2456179248692b72ad
Barrospaulo <pafbarros.sx@gmail.com>
2025-02-27 11:20:12 +0000

    Modal Plano-Semanal
    ..

--- TEC-Online/src/app/plano-semanal/plano-semanal.page.ts
+++ TEC-Online/src/app/plano-semanal/plano-semanal.page.ts
@@ -1,20 +1,39 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Importe o CommonModule para *ngFor
-import { FormsModule } from '@angular/forms'; // Importe o FormsModule para ngModel
-import { IonicModule } from '@ionic/angular'; // Importe o IonicModule para componentes do Ionic
-import { Router } from '@angular/router';
+import { Component, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule, IonModal } from '@ionic/angular';
+import { Router } from '@angular/router'; // Importe o Router
 
 @Component({
   selector: 'app-plano-semanal',
   templateUrl: './plano-semanal.page.html',
   styleUrls: ['./plano-semanal.page.scss'],
-  standalone: true, // Certifique-se de que o componente é standalone
+  standalone: true,
   imports: [
-    CommonModule, // Para usar *ngFor e outras diretivas do Angular
-    FormsModule,  // Para usar ngModel
-    IonicModule,  // Para usar componentes do Ionic
+    CommonModule,
+    FormsModule,
+    IonicModule,
   ],
 })
 export class PlanoSemanalPage {
+  @ViewChild('modal') modal!: IonModal;
 
+  constructor(private router: Router) {} // Injete o Router
+
+  // Método para abrir o modal
+  openModal(event: any) {
+    const selectedDate = event.detail.value;
+    console.log('Data selecionada:', selectedDate);
+    this.modal.present();
   }
+
+  // Método para fechar o modal
+  closeModal() {
+    this.modal.dismiss();
+  }
+
+  // Método para navegar para outra página
+  navigateToOtherPage() {
+    this.router.navigate(['/servicos']); // Altere '/outra-pagina' para o caminho da sua página
+  }
+}
