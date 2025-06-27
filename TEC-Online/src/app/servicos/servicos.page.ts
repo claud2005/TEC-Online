75ad96e2c333e30397b991c466801457a47d10e1
Barrospaulo <pafbarros.sx@gmail.com>
2025-06-27 16:48:24 +0100

    ..

--- TEC-Online/src/app/servicos/servicos.page.ts
+++ TEC-Online/src/app/servicos/servicos.page.ts
@@ -62,12 +62,12 @@ export class ServicosPage implements OnInit {
       const year = date.getFullYear();
       const hours = String(date.getHours()).padStart(2, '0');
       const minutes = String(date.getMinutes()).padStart(2, '0');
-      return `${year}-${month}-${day} ${hours}:${minutes}`;
+      return `${day}-${month}-${year} ${hours}:${minutes}`;
     } else {
-      const year = date.getFullYear();
-      const month = String(date.getMonth() + 1).padStart(2, '0');
       const day = String(date.getDate()).padStart(2, '0');
-      return `${year}-${month}-${day} 00:00`;
+      const month = String(date.getMonth() + 1).padStart(2, '0');
+      const year = date.getFullYear();
+      return `${day}-${month}-${year} 00:00`;
     }
   }
 
