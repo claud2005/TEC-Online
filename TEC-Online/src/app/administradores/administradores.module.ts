15d07ef64982ef20a325fe242dfedc596e619587
claud2005 <162583107+claud2005@users.noreply.github.com>
2025-06-30 11:47:28 +0100

    ...

--- TEC-Online/src/app/administradores/administradores.module.ts
+++ TEC-Online/src/app/administradores/administradores.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
+import { RouterModule, Routes } from '@angular/router';
 import { AdministradoresPage } from './administradores.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AdministradoresPage
+  }
+];
 
 @NgModule({
   imports: [
-    CommonModule,
-    IonicModule,
-    AdministradoresPage  // IMPORTANDO o standalone component aqui
+    RouterModule.forChild(routes),
+    AdministradoresPage  // Importa o standalone component
   ],
+  exports: [RouterModule]
 })
 export class AdministradoresPageModule {}
