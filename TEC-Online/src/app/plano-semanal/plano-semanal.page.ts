4dc80fb9db3ea750a60010e7e4f4a953deddacf2
Barrospaulo <pafbarros.sx@gmail.com>
2025-07-01 17:31:05 +0100

    ..

--- TEC-Online/src/app/plano-semanal/plano-semanal.page.ts
+++ TEC-Online/src/app/plano-semanal/plano-semanal.page.ts
@@ -46,13 +46,13 @@ export class PlanoSemanalPage implements OnInit {
 
   atualizarNomeUtilizador() {
     const storedUsername = localStorage.getItem('username');
-    this.utilizadorName = storedUsername ?? 'Utilizador'; // Usa nullish coalescing para segurança
+    this.utilizadorName = storedUsername ?? 'Utilizador';
   }
 
   verificarSeAdmin() {
     const role = localStorage.getItem('role');
     this.isAdmin = role === 'admin';
-    console.log('isAdmin:', this.isAdmin); // Para depurar se o valor está correto
+    console.log('isAdmin:', this.isAdmin);
   }
 
   carregarServicos() {
@@ -70,10 +70,10 @@ export class PlanoSemanalPage implements OnInit {
           if (statusAtual === 'concluído') statusAtual = 'fechado';
           return {
             id: servico._id,
-            nomeCompletoCliente: servico.cliente || 'Cliente não informado',
-            dataServico: servico.dataServico || '', // Use string vazia para evitar erros na conversão Date
-            problemaCliente: servico.descricao || 'Problema não descrito',
+            nomeCompletoCliente: servico.nomeCompletoCliente || 'Cliente não informado',
+            dataServico: servico.dataServico || '',
             horaServico: servico.horaServico || 'Horário não definido',
+            problemaRelatado: servico.problemaRelatado || 'Problema não descrito',
             status: statusAtual,
             autorServico: servico.autorServico || '',
             observacoes: servico.observacoes || '',
